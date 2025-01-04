46efdb21fea9ad5e7e1da7a3567b61b603592f2c
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2025-01-04 11:02:55 -0600

    Swapped CrudRepository for JpaRepository

--- src/main/java/us/gndwrk/manu/repositories/CustomerRepository.java
+++ src/main/java/us/gndwrk/manu/repositories/CustomerRepository.java
@@ -2,11 +2,11 @@ package us.gndwrk.manu.repositories;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import us.gndwrk.manu.entities.Customer;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
   Customer findById(long id);
 
   List<Customer> findByLastName(String lastName);
